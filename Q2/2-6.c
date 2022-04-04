321a7578f32dbddd3b3a9190653ea51cb3727d0c
Elin <elinlim22@gmail.com>
2022-04-04 17:42:24 +0900

    ahhhhh

--- Q2/2-6.c
+++ Q2/2-6.c
@@ -18,13 +18,26 @@ int main(int argc __attribute__((unused)), char * argv[]) {
             printf("*");
         }
         printf("\n");
+        for (int j = 0; j <= (n*(n-1)-1)-i; j++) {
+            printf("*");
+        }
+        printf("\n");
     }
-/*        
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < 2*(n-i-1)-1; j++) {
+            printf("*");
+        }
+        for (int j = 0; j <= 2*i; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < 2*(n-i-1)-1; j++) {
+            printf("*");
+        }
+        printf("\n");
     }
 
-
-
-    for (int i = 0; i < n-1; i++) 
-*/
     return 0;
 }
