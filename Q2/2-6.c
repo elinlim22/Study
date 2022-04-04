082042da018f3199ec8ccc5ca55bde03a0a81dac
Elin <elinlim22@gmail.com>
2022-04-04 17:02:34 +0900

    3rd

--- Q2/2-6.c
+++ Q2/2-6.c
@@ -4,7 +4,27 @@
 int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
 
-    for 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < 2*i+1; j++) {
+            printf("*");
+        }
+        for (int j = 0; j < 2*(n-i-1)-1; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j <= 2*i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+/*        
+    }
 
+
+
+    for (int i = 0; i < n-1; i++) 
+*/
     return 0;
 }
