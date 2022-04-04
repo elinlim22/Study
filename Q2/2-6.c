57118175d0cba7745634a36e504cce6ca68657cd
Elin <elinlim22@gmail.com>
2022-04-04 18:20:56 +0900

    3rd

--- Q2/2-6.c
+++ Q2/2-6.c
@@ -4,36 +4,32 @@
 int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { //상단
         for (int j = 0; j < n-i-1; j++) {
             printf(" ");
         }
-        for (int j = 0; j < 2*i+1; j++) {
+        for (int j = 0; j < (2*i+1); j++) {
             printf("*");
         }
-        for (int j = 0; j < 2*(n-i-1)-1; j++) {
+        for (int j = 0; j < 2*(n-i-1); j++) {
             printf(" ");
         }
-        for (int j = 0; j <= 2*i; j++) {
-            printf("*");
-        }
-        printf("\n");
-        for (int j = 0; j <= (n*(n-1)-1)-i; j++) {
+        for (int j = 0; j < (2*i+1); j++) {
             printf("*");
         }
         printf("\n");
     }
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < i; j++) {
+    for (int i = 0; i < n-1; i++) { //하단
+        for (int j = 0; j <= i; j++) {
             printf(" ");
         }
-        for (int j = 0; j < 2*(n-i-1)-1; j++) {
+        for (int j = 0; j < (2*(n-i-1)-1); j++) {
             printf("*");
         }
-        for (int j = 0; j <= 2*i; j++) {
+        for (int j = 0; j < 2*(i+1); j++) {
             printf(" ");
         }
-        for (int j = 0; j < 2*(n-i-1)-1; j++) {
+        for (int j = 0; j < (2*(n-i-1)-1); j++) {
             printf("*");
         }
         printf("\n");
