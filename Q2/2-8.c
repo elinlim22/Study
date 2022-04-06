557f28beece00bad74a6748e1bb34a09863d4957
Elin <elinlim22@gmail.com>
2022-04-06 11:19:24 +0900

    주석

--- Q2/2-8.c
+++ Q2/2-8.c
@@ -4,14 +4,14 @@
 int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { //상단
         for (int j = 0; j < 4*n-2-i; j++) //공백
             printf(" ");
         for (int j = 0; j < 2*i+1; j++) //상단 세모
             printf("*");
         printf("\n");
     }
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { //하단
         for (int j = 0; j < n-i-1; j++)  //Pattern 1-1
             printf(" ");
         for (int j = 0; j < 2*i+1; j++)  //Pattern 1-2
