e56695fc4bfe999d7d58f4224e4720fa7d481b66
Elin <elinlim22@gmail.com>
2022-04-10 20:49:35 +0900

    0410

--- Q3/3-8.c
+++ Q3/3-8.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc __attribute__((unused)), char * argv[]) {
+int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
     int arr[n][n];
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-                arr[i][j] = (i+j)%2==0;
+            arr[i][j] = (i%6<3 && j%6<3) ||
+                        !(i%6<3 || j%6<3);
         }
     }
 
--- Q3/3-9.c
+++ Q3/3-9.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc __attribute__((unused)), char * argv[]) {
+int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
     int arr[n][n];
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-                arr[i][j] = (i+j)%2==0;
-        }
-    }
+    
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -20,3 +16,4 @@ int main (int argc __attribute__((unused)), char * argv[]) {
 
     return 0;
 }
+
