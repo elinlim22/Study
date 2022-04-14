02efad3ce253a9558120f9060cba6a6a98228818
Elin <elinlim22@gmail.com>
2022-04-14 20:01:26 +0900

    hell

--- Q3/3-9.c
+++ Q3/3-9.c
@@ -5,17 +5,35 @@ int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
     int arr[n][n];
     int k = 1;
-    int a;
+    int a = 1;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i == 0; i++) {
         for (int j = 0; j < n; j++) {
             arr[i][j] = k++;
             a = j;
         }
-        for (int j = 1; j < n-i; j++) {
+    }
+//    printf("a is %d\n", a);
+
+    for (int i = 0; i == 0; i++) {
+        for (int j = i+1; j < n; j++) {
             arr[j][a] = k++;
         }
+        for (int j = i+1; j < n; j++) {
+            arr[a][n-j-1] = k++;
+        }
+        for (int j = i+1; j < n-i-1; j++) {
+            arr[n-j-1][i] = k++;
+        }
+        //break;
     }
+
+    for (int i = 1; i < n-1; i++) {
+        for (int j = 1; j < n-1; j++) {
+            arr[i][j] = 0;
+        }
+    }
+//    printf("a is %d\n", a);
     
 
 
