21dce6bfc8b20847eb6151557b7c88b3bbdbcc1b
Elin <elinlim22@gmail.com>
2022-04-14 18:53:45 +0900

    aaa

--- Q3/3-9.c
+++ Q3/3-9.c
@@ -5,12 +5,19 @@ int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
     int arr[n][n];
     int k = 1;
+    int a;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n-i; j++) {
+        for (int j = 0; j < n; j++) {
             arr[i][j] = k++;
-         }
+            a = j;
+        }
+        for (int j = 1; j < n-i; j++) {
+            arr[j][a] = k++;
+        }
     }
+    
+
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
