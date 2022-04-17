460d9c615de515ed6b57f2a790d0dff7a6800735
Elin <elinlim22@gmail.com>
2022-04-17 16:32:40 +0900

    .

--- Q3/3-11.c
+++ Q3/3-11.c
@@ -3,8 +3,30 @@
 
 int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
+    int k = n+2;
+    int arr[k][k];
 
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            if (i%(k-1)==0) {
+                arr[i][j] = n;
+                //arr[j][i] = n-i;
+            } 
+            else if ((i%(k-1))%2==1) {
+                arr[i][j] = n-1;
+            }
+            else {
+                arr[i][j] = n-2;
+            }
+        }
+    }
 
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            printf("%2d", arr[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 
