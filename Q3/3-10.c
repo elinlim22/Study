3389f20f0c1a5136c14ea995562bca3767fad7af
Elin <elinlim22@gmail.com>
2022-04-18 23:04:34 +0900

    maandag

--- Q3/3-10.c
+++ Q3/3-10.c
@@ -3,8 +3,48 @@
 
 int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
+    int arr[n][n];
+
+for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 || j == n-1) {
+                arr[i][j] = i+j+1;
+            } else if (i == n-1 || j == 0) {
+                arr[i][j] = (n-1)*4-(i+j)+1;
+            } else if (i == 1 || j == n-2) {
+                arr[i][j] = i+j-1+(n-1)*4;
+            } else if (i == n-2 || j == 1) {
+                arr[i][j] = (n-2)*4-(i+j)+(n-1)*4;
+            } else {
+                arr[i][j] = n*n+1;
+            }
+        }
+    }
 
 
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         if (i == 0 || j == n-1) {
+    //             arr[i][j] = i+j+1;
+    //         } else if (i == n-1 || j == 0) {
+    //             arr[i][j] = (n-1)*4-(i+j)+1;
+    //         } else if (i == 1 || j == n-2) {
+    //             arr[i][j] = i+j-1+(n-1)*4;
+    //         } else if (i == n-2 || j == 1) {
+    //             arr[i][j] = (n-2)*4-(i+j)+(n-1)*4;
+    //         } else {
+    //             arr[i][j] = n*n+1;
+    //         }
+    //     }
+    // }
+
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%3d", arr[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 
--- Q3/3-9-2.c
+++ Q3/3-9-2.c
@@ -7,16 +7,15 @@ int main(int argc __attribute__((unused)), char * argv[]) {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            arr[i][j] = 0;
+            arr[i][j] = i+j+1;
         }
     }
 
-    for (int i = 0; i < n-1; i++) {
-        arr[0][i] = i+1;
-        arr[i][n-1] = n+i;
-        arr[n-1][n-i-1] = n*2+i-1;
-        arr[n-i-1][0] = n*3+i-2; 
-    }
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         arr[i][j] = (n-1)*4-(i+j)+1;
+    //     }
+    // }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
