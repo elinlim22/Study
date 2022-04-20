0a444f4ed9f6aa0958c8e32098edfae2da972c48
Elin <elinlim22@gmail.com>
2022-04-20 22:03:41 +0900

    wed

--- Q3/3-11.c
+++ Q3/3-11.c
@@ -7,10 +7,12 @@ int main(int argc __attribute__((unused)), char * argv[]) {
     int arr[k][k];
 
     for (int i = 0; i < k; i++) {
-        for (int j = 0; j < k; j++) {
-            if (i%(k-1) == 0 || j%(k-1) == 0) {
-                arr[i][j] = n;
-            } else if ()
+        for (int j = 0; j < k-i*2; j++) {
+            arr[i][j+i] = arr[k-i-1][i+j] = arr[i+j][i] = arr[i+j][k-i-1] = n-i;
+            // arr[i][j+i] = n-i;
+            // arr[k-i-1][i+j] = n-i;
+            // arr[i+j][i] = n-i;
+            // arr[i+j][k-i-1] = n-i;
         }
     }
 
--- Q3/3-12.c
+++ Q3/3-12.c
@@ -3,8 +3,27 @@
 
 int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
+    int arr[n][n];
+    int k = 1;
 
+    for (int i = 0; i < n; i++) { //쓰레기값 보기싫음
+        for (int j = 0; j < n; j++) {
+            arr[i][j] = 0;
+        }
+    }
 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i+1; j++) {
+            arr[i-j][j] = k++;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%3d", arr[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 
