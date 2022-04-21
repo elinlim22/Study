0cdfa5c329f35fd85e622b37584a022f5a350ecc
Elin <elinlim22@gmail.com>
2022-04-21 21:29:31 +0900

    mmm

--- Q3/3-11.c
+++ Q3/3-11.c
@@ -21,14 +21,3 @@ int main(int argc __attribute__((unused)), char * argv[]) {
     return 0;
 
 }
-
-// if (i%(k-1)==0) {
-            //     arr[i][j] = n;
-            //     //arr[j][i] = n-i;
-            // } 
-            // else if ((i%(k-1))%2==1) {
-            //     arr[i][j] = n-1;
-            // }
-            // else {
-            //     arr[i][j] = n-2;
-            // }
--- Q4/4-1.c
+++ Q4/4-1.c
@@ -5,7 +5,34 @@ int main(int argc __attribute__((unused)), char * argv[]) {
     int n = atoi(argv[1]);
     int arr[n][n];
 
-MABANGJIN
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            arr[i][j] = 0;
+        }
+    }
+
+    int i = 0;
+    int j = n/2;
+    
+    for (int k = 1; k < n*n+1; k++, i--, j++) {
+        if (i < 0 && j > n-1) {
+            if (arr[i+n][j-n] != 0) {
+                i += 2;
+                j -= 1;
+            } else {
+                i += n;
+                j -= n;
+            }
+        } else if (i < 0) {
+            i += n;
+        } else if (j > n-1) {
+            j -= n;
+        } else if (arr[i][j] != 0) {
+            i += 2;
+            j -= 1;
+        }
+        arr[i][j] = k;
+    }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
