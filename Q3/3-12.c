86017c850ae702909adc46e8645e9435c0dda8f3
Elin <elinlim22@gmail.com>
2022-04-21 16:55:06 +0900

    asdf

--- Q3/3-12.c
+++ Q3/3-12.c
@@ -6,15 +6,16 @@ int main(int argc __attribute__((unused)), char * argv[]) {
     int arr[n][n];
     int k = 1;
 
-    for (int i = 0; i < n; i++) { //쓰레기값 보기싫음
-        for (int j = 0; j < n; j++) {
-            arr[i][j] = 0;
-        }
-    }
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i+1; j++) {
             arr[i-j][j] = k++;
+        }
+    }
+    k = n*n;
+
+    for (int i = n; i > 0; i--) {
+        for (int j = 0; j < n-i; j++) {
+            arr[i+j][n-j-1] = k--;
         }
     }
 
